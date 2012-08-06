ad7293b60a4c5ab690a2d8ebd5291f3be424c526
ljl8381 <lijinglei8381@163.com>
2012-08-06 22:08:57 +0800

    init project

---
+++ TmallSelection/TmallSelection/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TmallSelection
+//
+//  Created by ljl on 12-8-6.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TSAppDelegate class]));
+    }
+}
